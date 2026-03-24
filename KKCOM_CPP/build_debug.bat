22abc077e8ea03163a547ff48190ff5d9ce3130b
K.H. Sandoki <k.h.sandoki@gmail.com>
2026-03-25 00:02:51 +0800

    fix build bat files

--- KKCOM_CPP/build_debug.bat
+++ KKCOM_CPP/build_debug.bat
@@ -6,16 +6,6 @@ echo KKCOM Debug Build with Console
 echo ========================================
 echo.
 
-REM Setup Visual Studio 2019 BuildTools environment
-echo Setting up Visual Studio 2019 BuildTools environment...
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
-
-if %errorlevel% neq 0 (
-    echo Failed to setup Visual Studio environment!
-    pause
-    exit /b 1
-)
-
 REM Check if static libraries are available
 if not exist "vcpkg\installed\x64-windows-static\lib\glfw3.lib" (
     echo Error: Static GLFW3 library not found!
